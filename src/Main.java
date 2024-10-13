8617a0359ca8a4d3be709de810fc79cea827fb7f
Samuil Gantchev <samuil.gantchev@gmail.com>
2024-10-13 16:42:09 +0300

    Добавен прост Java код

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Моля въведете име на книга: "); //print
+
+        Scanner sc = new Scanner(System.in); //def
+        String bookName = sc.nextLine(); //read
+
+        System.out.println(bookName);
+    }
+}
